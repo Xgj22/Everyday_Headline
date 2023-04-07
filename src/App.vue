06aaf021e8c30e1811fbe370c92aa05b3501a508
unknown <2440167642@qq.com>
2023-04-07 20:53:02 +0800

    引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="app">
     <h1>天天头条</h1>
-    <router-view></router-view>
+    <div class="iconfont icon-shouji"></div>
+    <!-- <router-view></router-view> -->
+    <!-- <van-button type="primary">主要按钮</van-button>
+    <van-button type="success">成功按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button> -->
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 引入全局样式文件
+import '@/style/index.less'
+// 加入 vant 组件库
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
